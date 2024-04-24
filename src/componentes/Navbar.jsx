ffa077ed4cd01dbe27798f289f951a08bf295457
AlbaZ <albazuniga2202@gmail.com>
2024-04-24 17:20:47 -0600

    add

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -18,7 +18,7 @@ function Navbar({ user }) {
   return (
     <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">API Super Heroes</a>
+        <a className="navbar-brand" href="#">API </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
